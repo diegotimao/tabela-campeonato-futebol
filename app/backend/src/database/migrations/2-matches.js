1d38df5344d3fee00274cede5ee0653de9eec377
diegotimao <diegotimao1104@gmail.com>
2022-10-01 03:45:27 -0500

    fix: Alterando migrations de matches

---
+++ app/backend/src/database/migrations/2-matches.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: async (QueryInterface, Sequelize) => {
+    await QueryInterface.createTable('matches',  {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      homeTeam: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        field: 'home_team'
+      },
+      homeTeamGols: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'home_team_goals'
+      },
+      awayTeam: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'away_team',
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      awayTeamGoals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'away_team_goals'
+      },
+      inProgress: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        field: 'in_progress'
+      }
+    })
+  },
+  down: async (QueryInterface) => {
+    await QueryInterface.dropTable('matches');
+  }
+}
