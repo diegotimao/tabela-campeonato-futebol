6609cfd3080d436e8189302874078ee3f6169203
diegotimao <diegotimao1104@gmail.com>
2022-10-01 03:45:49 -0500

    fix: Alterando migration de user

---
+++ app/backend/src/database/migrations/3-users-login.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      userName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'username',
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      }
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users');
+  },
+};
