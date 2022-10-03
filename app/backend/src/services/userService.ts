563f9a25086b8953d27a52091a3277fc06eb9260
diegotimao <diegotimao1104@gmail.com>
2022-10-03 15:41:36 -0500

    feat: Adicionando userService de user

---
+++ app/backend/src/services/userService.ts
@@ -0,0 +1,35 @@
+import { compareSync } from 'bcryptjs';
+import AuthToken from '../utils/authToken';
+import Exeption from '../utils/exeption';
+import LoginDTO from '../dto/loginDto';
+import User from '../database/models/user';
+import IHeader from '../dto/headerDto';
+
+export default class UserService {
+  public static async autentication(loginDTO: LoginDTO) {
+    if (!loginDTO.email || !loginDTO.password) throw new Exeption(400, 'All fields must be filled');
+
+    const user = await User.findOne({
+      attributes: ['email', 'password'],
+      where: { email: loginDTO.email },
+    });
+
+    if (!user) throw new Exeption(401, 'Incorrect email or password');
+
+    const headerJWT: IHeader = {
+      id: user.getDataValue('id'),
+      username: user.getDataValue('username'),
+      email: user.getDataValue('email'),
+      role: user.getDataValue('role'),
+      password: user.getDataValue('password'),
+    };
+
+    const isHasch = compareSync(loginDTO.password, headerJWT.password);
+
+    if (!isHasch) throw new Exeption(401, 'Incorrect email or password');
+
+    const tokenCreate = new AuthToken();
+    const token = tokenCreate.gerarToken(headerJWT);
+    return { token };
+  }
+}
