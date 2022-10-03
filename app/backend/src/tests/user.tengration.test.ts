d22a68974f6cb8eac7f21517c43a0e171b6c998a
diegotimao <diegotimao1104@gmail.com>
2022-10-03 15:43:10 -0500

    feat: Estrutura de teste login user

---
+++ app/backend/src/tests/user.tengration.test.ts
@@ -0,0 +1,10 @@
+import * as chai from 'chai';
+import chaiHttp from 'chai-http';
+
+chai.use(chaiHttp);
+
+describe('Testando a rota login', () => {
+  describe('Testando o metodo POST', async () => {
+    const response = await chai.request();
+  })
+});
