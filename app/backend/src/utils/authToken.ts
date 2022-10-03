3c6b2410be0771ccb64f5d79bf519675109d327d
diegotimao <diegotimao1104@gmail.com>
2022-10-03 15:40:10 -0500

    feat: Adicionando classe que trabalha o token

---
+++ app/backend/src/utils/authToken.ts
@@ -0,0 +1,36 @@
+import * as Jwt from 'jsonwebtoken';
+import { SignOptions } from 'jsonwebtoken';
+import IHeader from '../dto/headerDto';
+import Exeption from './exeption';
+
+const SECRET = 'diego';
+
+const jwtDefaultConfig: SignOptions = {
+  expiresIn: '2d',
+  algorithm: 'HS256',
+};
+
+export default class AuthToken {
+  constructor(private jwtConfig?: SignOptions) {
+    if (!jwtConfig) {
+      this.jwtConfig = jwtDefaultConfig;
+    }
+  }
+
+  public gerarToken(payload: IHeader) {
+    return Jwt.sign(payload, SECRET, this.jwtConfig);
+  }
+
+  public async autenticateToken(token: string) {
+    if (!token) {
+      throw new Exeption(401, 'Token notfound.');
+    }
+
+    try {
+      const autorization = Jwt.verify(token, SECRET, this.jwtConfig);
+      return autorization;
+    } catch (error) {
+      throw new Exeption(401, 'Token invalid');
+    }
+  }
+}
