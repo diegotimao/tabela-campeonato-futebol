03e995f799cf50f5d8b2f78a489de76a51c3495e
diegotimao <diegotimao1104@gmail.com>
2022-10-03 20:01:26 -0500

    feat: Adicionando tratamento de erro

--- app/backend/src/controller/userController.ts
+++ app/backend/src/controller/userController.ts
@@ -1,10 +1,17 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import UserService from '../services/userService';
 
 class UserController {
-  public static async login(req: Request, res: Response) {
-    const token = UserService.autentication(req.body);
-    return res.status(200).json(token);
+  public static async login(req: Request, res: Response, next: NextFunction) {
+    // const token = await UserService.autentication(req.body);
+    // return res.status(200).json(token);
+
+    try {
+      const token = await UserService.autentication(req.body);
+      return res.status(200).json(token);
+    } catch (error) {
+      return next(error);
+    }
   }
 }
 
