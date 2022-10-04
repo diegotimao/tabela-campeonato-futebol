520da153d192d98dcfd854c5116561af6f6edc12
diegotimao <diegotimao1104@gmail.com>
2022-10-03 20:04:15 -0500

    fix: Descomentando o parametro next

--- app/backend/src/middleware/error.middleware.ts
+++ app/backend/src/middleware/error.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import Exeption from '../utils/exeption';
 
-const errorMiddleware = (err: Error, req: Request, res: Response, _next: NextFunction) => {
+const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
   const { status, message } = err as Exeption;
   res.status(status || 500).json({ message });
 };
