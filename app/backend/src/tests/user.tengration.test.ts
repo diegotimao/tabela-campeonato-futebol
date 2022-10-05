8377e0ba722ced70d81d0a339b87155393cabdbe
diegotimao <diegotimao1104@gmail.com>
2022-10-04 19:50:04 -0500

    test: Testando a rota de login/validate

--- app/backend/src/tests/user.tengration.test.ts
+++ app/backend/src/tests/user.tengration.test.ts
@@ -1,11 +1,20 @@
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http'); 
+import * as Sinon from 'sinon';
 import { app } from '../app';
+import LoginDTO from '../dto/loginDto';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
+
+const useMock: LoginDTO = {
+  email: 'admin@admin.com',
+  password: 'secret_admin'
+}
+
+const tokenMock: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicm9sZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDgkeGkuSHhrMWN6QU8wblpSLi5CMzkzdTEwYUVEMFJRMU4zUEFFWFE3SHh0TGpLUEVaQnUuUFciLCJpYXQiOjE2NjQ5MjI3NjQsImV4cCI6MTY2NTA5NTU2NH0.ZnmO08srhmoBRpQk2V7K-jTamJl4Vq62x6Gr-teXogk'
 
 describe('Testando o metodo POST', async () => {
   it('Deve retornar o status 400, se o email não for passada.', async () => {
@@ -16,7 +25,6 @@ describe('Testando o metodo POST', async () => {
     expect(result).to.have.status(400);
   });
 
-
   it('Deve retornar o status 400, se a senha não for passada.', async () => {
     const result = await chai.request(app).post('/login').send({
       email: 'admin@admin.com',
@@ -24,4 +32,17 @@ describe('Testando o metodo POST', async () => {
     });
     expect(result).to.have.status(400);
   });
+
+  it('Deve retornar o status 200', async () => {
+    const response = await chai.request(app).post('/login').send(useMock);
+
+    expect(response).to.have.status(200);
+  })
+
+  it('Testando a rota validate', async () => {
+    const response = await 
+    chai.request(app).get('/login/validate').set('autorization', 'Bearer' + tokenMock);
+
+    expect(response.body).to.be.a('object');
+  })
 });
