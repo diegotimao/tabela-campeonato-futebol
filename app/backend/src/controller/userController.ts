c99c6236d19db1b8764a2ea28edffa154a597ec0
diegotimao <diegotimao1104@gmail.com>
2022-10-05 17:56:18 -0500

    refactor: Separando responsabilidade userControll

--- app/backend/src/controller/userController.ts
+++ app/backend/src/controller/userController.ts
@@ -3,20 +3,22 @@ import Exeption from '../utils/exeption';
 import UserService from '../services/userService';
 
 class UserController {
-  public static async login(req: Request, res: Response, next: NextFunction) {
+  constructor(private userService: UserService) {}
+
+  async login(req: Request, res: Response, next: NextFunction) {
     try {
-      const token = await UserService.autentication(req.body);
+      const token = await this.userService.autentication(req.body);
       return res.status(200).json(token);
     } catch (error) {
       return next(error);
     }
   }
 
-  public static async validate(req: Request, res: Response, next: NextFunction) {
+  async validate(req: Request, res: Response, next: NextFunction) {
     try {
       const token = req.header('authorization');
       if (!token) throw new Exeption(400, 'token notgound');
-      const role = await UserService.validate(token);
+      const role = await this.userService.validate(token);
       return res.status(200).json({ role });
     } catch (error) {
       return next(error);
