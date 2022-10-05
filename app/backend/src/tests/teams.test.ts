692dc1e9ab435e753f3f2bd2ad280a094f0bfd61
diegotimao <diegotimao1104@gmail.com>
2022-10-05 17:59:15 -0500

    test: Testando as funcionalidades da rota teams

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,16 @@
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http'); 
+// import * as Sinon from 'sinon';
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testando as funcionalidades das rotas de Teams GET', () => {
+  it('Ao acessar a /teams, é esperando o status 200', async () => {
+    const result = await chai.request(app).get('/teams');
+    expect(result).to.have.status(200);
+  });
+})
