880c1d5bf29a4d0349c4e6416deed57b0e721e21
diegotimao <diegotimao1104@gmail.com>
2022-10-04 19:47:06 -0500

    feat: Adiciona controller login/validate

--- app/backend/src/controller/userController.ts
+++ app/backend/src/controller/userController.ts
@@ -1,14 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
+import Exeption from '../utils/exeption';
 import UserService from '../services/userService';
 
 class UserController {
   public static async login(req: Request, res: Response, next: NextFunction) {
-    // const token = await UserService.autentication(req.body);
-    // return res.status(200).json(token);
-
     try {
       const token = await UserService.autentication(req.body);
       return res.status(200).json(token);
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  public static async validate(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = req.header('authorization');
+      if (!token) throw new Exeption(400, 'token notgound');
+      const role = await UserService.validate(token);
+      return res.status(200).json({ role });
     } catch (error) {
       return next(error);
     }
