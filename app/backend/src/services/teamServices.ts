836bb64fb96033d1c933360009216c03da18d8ec
diegotimao <diegotimao1104@gmail.com>
2022-10-05 11:27:30 -0500

    feat: Adicionando teamService

---
+++ app/backend/src/services/teamServices.ts
@@ -0,0 +1,12 @@
+import Exeption from '../utils/exeption';
+import Teams from '../database/models/teams';
+
+export default class TeamService {
+  public static async getAll() {
+    const teamsAll = await Teams.findAll();
+
+    if (teamsAll.length <= 0) throw new Exeption(400, 'Notfound');
+
+    return teamsAll;
+  }
+}
