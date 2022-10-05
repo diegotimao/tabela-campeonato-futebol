bcb457555ae1daaff56b15aba0a314a72668f007
diegotimao <diegotimao1104@gmail.com>
2022-10-05 17:54:59 -0500

    refactor: Separando responsabilidadeteamControlle

--- app/backend/src/controller/teamsController.ts
+++ app/backend/src/controller/teamsController.ts
@@ -2,10 +2,21 @@ import { Request, Response, NextFunction } from 'express';
 import TeamService from '../services/teamServices';
 
 export default class TeamsController {
-  public static async getAll(req: Request, res: Response, next: NextFunction) {
+  constructor(private teamsService: TeamService) {}
+
+  async getAll(req: Request, res: Response, next: NextFunction) {
     try {
-      const teamsAll = await TeamService.getAll();
+      const teamsAll = await this.teamsService.getAll();
       return res.status(200).json(teamsAll);
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  async getOne(req: Request, res: Response, next: NextFunction) {
+    try {
+      const oneTeam = await this.teamsService.getOne(Number(req.params.id));
+      return res.status(200).json(oneTeam);
     } catch (error) {
       return next(error);
     }
