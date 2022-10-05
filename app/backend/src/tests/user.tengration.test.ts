b88226fb25259bd6727c513d05c7d1fa51affa13
diegotimao <diegotimao1104@gmail.com>
2022-10-05 18:01:43 -0500

    test: Testando os casos de erro com o token

--- app/backend/src/tests/user.tengration.test.ts
+++ app/backend/src/tests/user.tengration.test.ts
@@ -1,7 +1,7 @@
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http'); 
-import * as Sinon from 'sinon';
+// import * as Sinon from 'sinon';
 import { app } from '../app';
 import LoginDTO from '../dto/loginDto';
 
@@ -14,7 +14,8 @@ const useMock: LoginDTO = {
   password: 'secret_admin'
 }
 
-const tokenMock: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicm9sZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDgkeGkuSHhrMWN6QU8wblpSLi5CMzkzdTEwYUVEMFJRMU4zUEFFWFE3SHh0TGpLUEVaQnUuUFciLCJpYXQiOjE2NjQ5MjI3NjQsImV4cCI6MTY2NTA5NTU2NH0.ZnmO08srhmoBRpQk2V7K-jTamJl4Vq62x6Gr-teXogk'
+const tokenMockValid: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicm9sZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDgkeGkuSHhrMWN6QU8wblpSLi5CMzkzdTEwYUVEMFJRMU4zUEFFWFE3SHh0TGpLUEVaQnUuUFciLCJpYXQiOjE2NjQ5MjI3NjQsImV4cCI6MTY2NTA5NTU2NH0.ZnmO08srhmoBRpQk2V7K-jTamJl4Vq62x6Gr-teXogk'
+const tokenMockInvalid: string = 'eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicm9sZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDgkeGkuSHhrMWN6QU8wblpSLi5CMzkzdTEwYUVEMFJRMU4zUEFFWFE3SHh0TGpLUEVaQnUuUFciLCJpYXQiOjE2NjQ5MjI3NjQsImV4cCI6MTY2NTA5NTU2NH0.ZnmO08srhmoBRpQk2VK-jTamJl4Vq62x6Gr-teXogk'
 
 describe('Testando o metodo POST', async () => {
   it('Deve retornar o status 400, se o email não for passada.', async () => {
@@ -37,12 +38,27 @@ describe('Testando o metodo POST', async () => {
     const response = await chai.request(app).post('/login').send(useMock);
 
     expect(response).to.have.status(200);
-  })
+  });
 
-  it('Testando a rota validate', async () => {
+  it('Testando a rota validate com um token válido', async () => {
     const response = await 
-    chai.request(app).get('/login/validate').set('autorization', 'Bearer' + tokenMock);
+    chai.request(app).get('/login/validate').set('autorization', 'Bearer' + tokenMockValid);
 
     expect(response.body).to.be.a('object');
-  })
+  });
+
+  
+  it('Testando a rota validate sem o token', async () => {
+    const response = await 
+    chai.request(app).get('/login/validate').set('autorization', 'Bearer' + '');
+    
+    expect(response).to.have.status(400);
+  });
+  
+  it('Testando a rota validate com token inválido', async () => {
+    const response = await 
+    chai.request(app).get('/login/validate').set('autorization', 'Bearer' + tokenMockInvalid);
+
+    expect(response).to.have.status(400);
+  });
 });
