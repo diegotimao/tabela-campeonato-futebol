b188d977646874512b80344927a69612674b8f65
diegotimao <diegotimao1104@gmail.com>
2022-10-07 13:20:22 -0500

    feat: Adicionando verificação de rota

--- app/backend/src/controller/matchesController.ts
+++ app/backend/src/controller/matchesController.ts
@@ -5,7 +5,12 @@ export default class MatchesController {
   constructor(private matheServices: MatchesServices) {}
 
   async getMatches(req: Request, res: Response, next: NextFunction) {
-    try {
+    try { // ?inProgress=true
+      const query = req.query.inProgress;
+      if (typeof query === 'string') {
+        const response = await this.matheServices.getMatchesProgress(JSON.parse(query));
+        return res.status(200).json(response);
+      }
       const response = await this.matheServices.getMatches();
       return res.status(200).json(response);
     } catch (error) {
