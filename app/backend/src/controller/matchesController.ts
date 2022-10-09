d40ffc432613cee68f640fb66f5ce0d62789e8ea
diegotimao <diegotimao1104@gmail.com>
2022-10-09 16:28:29 -0500

    fix: Corrigindo erro

--- app/backend/src/controller/matchesController.ts
+++ app/backend/src/controller/matchesController.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
+// import Exeption from '../utils/exeption';
 import MatchesServices from '../services/matcheServices';
+// import AuthToken from '../utils/authToken';
 
 export default class MatchesController {
   constructor(private matheServices: MatchesServices) {}
@@ -17,4 +19,17 @@ export default class MatchesController {
       return next(error);
     }
   }
+
+  // async createMatches(req: Request, res: Response, next: NextFunction) {
+  //   try {
+  //     const token = req.header('authorization');
+  //     if (!token) throw new Exeption(400, 'token notgound');
+  //     const auth = new AuthToken();
+
+  //     const isAutorized = await auth.autenticateToken(token);
+  //     if (isAutorized) res.status(200).json(req.body);
+  //   } catch (error) {
+  //     return next(error);
+  //   }
+  // }
 }
