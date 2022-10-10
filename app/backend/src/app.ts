ba4d8b7c1eb072ea9271d67e67d8568e9b34bf6b
diegotimao <diegotimao1104@gmail.com>
2022-10-09 20:41:49 -0500

    refactor: Retirando as rotas do app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,15 +1,6 @@
 import * as express from 'express';
-import TeamsController from './controller/teamsController';
-import UserService from './services/userService';
-import UserController from './controller/userController';
-import User from './database/models/user';
 import errorMiddleware from './middleware/error.middleware';
-import AuthToken from './utils/authToken';
-import TeamService from './services/teamServices';
-import Teams from './database/models/teams';
-import MatchesController from './controller/matchesController';
-import MatchesServices from './services/matcheServices';
-import Matches from './database/models/matches';
+import routes from './routes';
 
 class App {
   public app: express.Express;
@@ -31,17 +22,7 @@ class App {
     };
     this.app.use(express.json());
     this.app.use(accessControl);
-
-    const auth = new AuthToken();
-    const userController = new UserController(new UserService(User, auth));
-    const teamsController = new TeamsController(new TeamService(Teams));
-    const matchesController = new MatchesController(new MatchesServices(Matches));
-    this.app.post('/login', (req, res, next) => userController.login(req, res, next));
-    this.app.get('/login/validate', (req, res, next) => userController.validate(req, res, next));
-    this.app.get('/teams', (req, res, next) => teamsController.getAll(req, res, next));
-    this.app.get('/teams/:id', (req, res, next) => teamsController.getOne(req, res, next));
-    this.app.get('/matches', (req, res, next) => matchesController.getMatches(req, res, next));
-    // this.app.post('/matches', (req, res, next) => matchesController.createMatches(req, res, next));
+    this.app.use(routes);
     this.app.use(errorMiddleware);
   }
 
