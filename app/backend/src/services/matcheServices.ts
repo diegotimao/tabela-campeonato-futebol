180c0fbaa3f2aacb693fc63c80e02d2d4981d132
diegotimao <diegotimao1104@gmail.com>
2022-10-14 13:44:06 -0500

    refactor: Retirando código refente a leadBoard

--- app/backend/src/services/matcheServices.ts
+++ app/backend/src/services/matcheServices.ts
@@ -4,20 +4,8 @@ import Exeption from '../utils/exeption';
 import Matches from '../database/models/matches';
 import IMatchesGoals from '../dto/updatedGoalsDTO';
 
-interface Table {
-  name: string;
-  totalPoints: number;
-  totalGames: number;
-  totalVictories: number;
-  totalDraws: number;
-  totalLosses: number;
-  goalsFavor: number;
-  goalsOwn: number;
-  goalsBalance: number;
-  efficiency: number;
-}
 export default class MatchesServices {
-  constructor(private matchesModel: typeof Matches, private teamsModel: typeof Teams) {}
+  constructor(private matchesModel: typeof Matches) {}
 
   async getMatches(): Promise<Matches[]> {
     const response = await this.matchesModel.findAll({
@@ -79,117 +67,5 @@ export default class MatchesServices {
 
     if (insertId === 0) throw new Exeption(400, 'Matches does not exist.');
     return insertId;
-  }
-
-  async getMatchesHome() {
-    const response = await this.teamsModel.findAll({
-      include: [
-        { model: Matches, as: 'teamHome', where: { inProgress: false } },
-      ],
-    });
-    return MatchesServices.matchesTable(response as unknown as Teams[]);
-  }
-
-  private static matchesTable(teams: Teams[]) {
-    const table = teams.map((team) => {
-      const name = team.teamName;
-      const matches = team.teamHome;
-      const resultTeam = MatchesServices.calculatorTable(name, matches);
-      return resultTeam;
-    });
-
-    return MatchesServices.matcheTableCassific(table);
-  }
-
-  public static matcheTableCassific(table: Table[]) {
-    const tableSort = table.sort((a, b) => {
-      if (a.totalPoints < b.totalPoints) return 1;
-      if (a.totalPoints > b.totalPoints) return -1;
-      if (a.totalVictories < b.totalVictories) return 1;
-      if (a.totalVictories > b.totalVictories) return -1;
-      if (a.goalsBalance < b.goalsBalance) return 1;
-      if (a.goalsBalance > b.goalsBalance) return -1;
-      if (a.goalsFavor < b.goalsFavor) return 1;
-      if (a.goalsFavor > b.goalsFavor) return -1;
-      return 0;
-    });
-    return tableSort;
-  }
-
-  public static calculatorTable(nameTeam: string, matches: Matches[]) {
-    const table = {
-      name: nameTeam,
-      totalPoints: MatchesServices.calculatorPoints(matches),
-      totalGames: MatchesServices.calculatorTotalGames(matches),
-      totalVictories: MatchesServices.calculatorTotalVictories(matches),
-      totalDraws: MatchesServices.calculatorDraws(matches),
-      totalLosses: MatchesServices.calculatorLosses(matches),
-      goalsFavor: MatchesServices.calculatorGoalsHome(matches),
-      goalsOwn: MatchesServices.calculatorGoalsOwn(matches),
-      goalsBalance: MatchesServices.calculatorGoalsBalance(matches),
-      efficiency: MatchesServices.calculatorEffieciency(matches),
-    };
-    return table;
-  }
-
-  public static calculatorTotalGames(matches: Matches[]) {
-    const totalGames = matches.length;
-    return totalGames;
-  }
-
-  public static calculatorTotalVictories(matches: Matches[]) {
-    const totalVictories = matches.filter((match) => match.homeTeamGoals > match.awayTeamGoals);
-    const total = totalVictories.length;
-    return total;
-  }
-
-  public static calculatorPoints(matches: Matches[]) {
-    const victories = matches.filter((match) => match.homeTeamGoals > match.awayTeamGoals);
-    const totalVictories = victories.length;
-    const draws = matches.filter((match) => match.homeTeamGoals === match.awayTeamGoals);
-    const totalDraws = draws.length;
-    const totalPoints = totalVictories * 3 + totalDraws;
-    return totalPoints;
-  }
-
-  public static calculatorDraws(matches: Matches[]) {
-    const draws = matches.filter((match) => match.homeTeamGoals === match.awayTeamGoals);
-    const totalDraws = draws.length;
-    return totalDraws;
-  }
-
-  public static calculatorLosses(matches: Matches[]) {
-    const losses = matches.filter((match) => match.homeTeamGoals < match.awayTeamGoals);
-    const totalLosses = losses.length;
-    return totalLosses;
-  }
-
-  public static calculatorGoalsHome(matches: Matches[]) {
-    const goals = matches.map((match) => match.homeTeamGoals);
-    const totalGoals = goals.reduce((prev, curr) => prev + curr);
-    return totalGoals;
-  }
-
-  public static calculatorGoalsOwn(matches: Matches[]) {
-    const goals = matches.map((match) => match.awayTeamGoals);
-    const totalGoals = goals.reduce((curr, prev) => curr + prev);
-    return totalGoals;
-  }
-
-  public static calculatorGoalsBalance(matches: Matches[]) {
-    const goalsHome = matches.map((match) => match.homeTeamGoals);
-    const totalGoalsHome = goalsHome.reduce((prev, curr) => prev + curr);
-    const goalsOwn = matches.map((match) => match.awayTeamGoals);
-    const totalGoalsOwn = goalsOwn.reduce((curr, prev) => curr + prev);
-    return totalGoalsHome - totalGoalsOwn;
-  }
-
-  public static calculatorEffieciency(matches: Matches[]) {
-    const totalGames = matches.length;
-    const victories = matches.filter((match) => match.homeTeamGoals > match.awayTeamGoals);
-    const losses = matches.filter((match) => match.homeTeamGoals === match.awayTeamGoals);
-    const totaLosses = losses.length;
-    const totalVictories = victories.length;
-    return Number((((totalVictories * 3 + totaLosses) / (totalGames * 3)) * 100).toFixed(2));
   }
 }
