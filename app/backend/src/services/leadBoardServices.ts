16bc4a099a4262d4e4ea3eea091b5e9180e078fd
diegotimao <diegotimao1104@gmail.com>
2022-10-14 14:46:26 -0500

    feat: Adicionando o service de buscar os visitante

--- app/backend/src/services/leadBoardServices.ts
+++ app/backend/src/services/leadBoardServices.ts
@@ -11,14 +11,35 @@ export default class LeadBoardServices {
         { model: Matches, as: 'teamHome', where: { inProgress: false } },
       ],
     });
-    return LeadBoardServices.matchesTable(response as unknown as Teams[]);
+    return LeadBoardServices.leadBoardTableHome(response as unknown as Teams[]);
   }
 
-  private static matchesTable(teams: Teams[]) {
+  async leadBoardAway() {
+    const response = await this.teamsModel.findAll({
+      include: [
+        { model: Matches, as: 'teamAway', where: { inProgress: false } },
+      ],
+    });
+
+    return LeadBoardServices.leadBoardTableAway(response);
+  }
+
+  private static leadBoardTableHome(teams: Teams[]) {
     const table = teams.map((team) => {
       const name = team.teamName;
-      const matches = team.teamHome;
-      const resultTeam = LeadBoardServices.calculatorTableHome(name, matches);
+      const draws = team.teamHome;
+      const resultTeam = LeadBoardServices.calculatorTableHome(name, draws);
+      return resultTeam;
+    });
+
+    return LeadBoardServices.matcheTableCassific(table);
+  }
+
+  private static leadBoardTableAway(teams: Teams[]) {
+    const table = teams.map((team) => {
+      const name = team.teamName;
+      const draws = team.teamAway;
+      const resultTeam = LeadBoardServices.calculatorTableAway(name, draws);
       return resultTeam;
     });
 
@@ -40,14 +61,15 @@ export default class LeadBoardServices {
     return tableSort;
   }
 
+  // refazer
   public static calculatorTableHome(nameTeam: string, matches: Matches[]) {
     const table = {
       name: nameTeam,
-      totalPoints: LeadBoardServices.calculatorPoints(matches),
+      totalPoints: LeadBoardServices.calculatorPointsHome(matches),
       totalGames: LeadBoardServices.calculatorTotalGames(matches),
-      totalVictories: LeadBoardServices.calculatorTotalVictories(matches),
+      totalVictories: LeadBoardServices.calculatorTotalVictoriesHome(matches),
       totalDraws: LeadBoardServices.calculatorDraws(matches),
-      totalLosses: LeadBoardServices.calculatorLosses(matches),
+      totalLosses: LeadBoardServices.calculatorLossesHome(matches),
       goalsFavor: LeadBoardServices.calculatorGoalsHome(matches),
       goalsOwn: LeadBoardServices.calculatorGoalsOwn(matches),
       goalsBalance: LeadBoardServices.calculatorGoalsBalance(matches),
@@ -56,18 +78,44 @@ export default class LeadBoardServices {
     return table;
   }
 
+  public static calculatorTableAway(nameTeam: string, matches: Matches[]) {
+    const table = {
+      name: nameTeam,
+      totalPoints: LeadBoardServices.calculatorPointsAway(matches),
+      totalGames: LeadBoardServices.calculatorTotalGames(matches),
+      totalVictories: LeadBoardServices.calculatorTotalVictoriesAway(matches),
+      totalDraws: LeadBoardServices.calculatorDraws(matches),
+      totalLosses: LeadBoardServices.calculatorLossesAway(matches),
+      goalsFavor: LeadBoardServices.calculatorGoalsHome(matches),
+      goalsOwn: LeadBoardServices.calculatorGoalsOwn(matches),
+      goalsBalance: LeadBoardServices.calculatorGoalsBalance(matches),
+      efficiency: LeadBoardServices.calculatorEffieciency(matches),
+    };
+    return table;
+  }
+
+  // Não refazer
   public static calculatorTotalGames(matches: Matches[]) {
     const totalGames = matches.length;
     return totalGames;
   }
 
-  public static calculatorTotalVictories(matches: Matches[]) {
+  // home
+  public static calculatorTotalVictoriesHome(matches: Matches[]) {
     const totalVictories = matches.filter((match) => match.homeTeamGoals > match.awayTeamGoals);
     const total = totalVictories.length;
     return total;
   }
 
-  public static calculatorPoints(matches: Matches[]) {
+  // away
+  public static calculatorTotalVictoriesAway(matches: Matches[]) {
+    const totalVictories = matches.filter((match) => match.awayTeamGoals > match.homeTeamGoals);
+    const total = totalVictories.length;
+    return total;
+  }
+
+  // home
+  public static calculatorPointsHome(matches: Matches[]) {
     const victories = matches.filter((match) => match.homeTeamGoals > match.awayTeamGoals);
     const totalVictories = victories.length;
     const draws = matches.filter((match) => match.homeTeamGoals === match.awayTeamGoals);
@@ -76,30 +124,51 @@ export default class LeadBoardServices {
     return totalPoints;
   }
 
+  // away
+  public static calculatorPointsAway(matches: Matches[]) {
+    const victories = matches.filter((match) => match.awayTeamGoals > match.homeTeamGoals);
+    const totalVictories = victories.length;
+    const draws = matches.filter((match) => match.homeTeamGoals === match.awayTeamGoals);
+    const totalDraws = draws.length;
+    const totalPoints = totalVictories * 3 + totalDraws;
+    return totalPoints;
+  }
+
+  // não precisa
   public static calculatorDraws(matches: Matches[]) {
     const draws = matches.filter((match) => match.homeTeamGoals === match.awayTeamGoals);
     const totalDraws = draws.length;
     return totalDraws;
   }
 
-  public static calculatorLosses(matches: Matches[]) {
+  // refazer
+  public static calculatorLossesHome(matches: Matches[]) {
     const losses = matches.filter((match) => match.homeTeamGoals < match.awayTeamGoals);
     const totalLosses = losses.length;
     return totalLosses;
   }
 
+  public static calculatorLossesAway(matches: Matches[]) {
+    const losses = matches.filter((match) => match.awayTeamGoals > match.homeTeamGoals);
+    const totalLosses = losses.length;
+    return totalLosses;
+  }
+
+  // não refazer
   public static calculatorGoalsHome(matches: Matches[]) {
     const goals = matches.map((match) => match.homeTeamGoals);
     const totalGoals = goals.reduce((prev, curr) => prev + curr);
     return totalGoals;
   }
 
+  // não refazer
   public static calculatorGoalsOwn(matches: Matches[]) {
     const goals = matches.map((match) => match.awayTeamGoals);
     const totalGoals = goals.reduce((curr, prev) => curr + prev);
     return totalGoals;
   }
 
+  // não refazer
   public static calculatorGoalsBalance(matches: Matches[]) {
     const goalsHome = matches.map((match) => match.homeTeamGoals);
     const totalGoalsHome = goalsHome.reduce((prev, curr) => prev + curr);
