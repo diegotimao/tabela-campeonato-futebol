41157cd1510d2e97bd67067b4feed9034d5eb831
diegotimao <diegotimao1104@gmail.com>
2022-10-17 11:41:09 -0500

    test: Testando a rota leadBoardAway

---
+++ app/backend/src/tests/leadBoardAway.test.ts
@@ -0,0 +1,17 @@
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http'); 
+import * as Sinon from 'sinon';
+import { app } from '../app';
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testando a rota leadBoard/away', () => {
+  it('Testando se retorna status 200', async () => {
+    const response = await chai.request(app).get('/leaderboard/away');
+    expect(response).to.have.status(200);
+  })
+})
